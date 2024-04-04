ebf09647b08637ab1b6a8630139c0b6cebbda16a
kiramitt <kiramitt@gmail.com>
2024-04-04 18:02:42 +0300

    /

--- src/test/java/Lessons/Lesson17_Test.java
+++ src/test/java/Lessons/Lesson17_Test.java
@@ -5,6 +5,6 @@ import org.testng.annotations.Test;
 public class Lesson17_Test {
     @Test
     public void test() {
-        System.out.println("Test");
+        System.out.println("Test1");
     }
 }
