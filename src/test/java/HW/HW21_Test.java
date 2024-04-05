951a053af0b7944847f7d43e006b228534c59c36
kiramitt <kiramitt@gmail.com>
2024-04-05 07:08:54 +0300

    /

---
+++ src/test/java/HW/HW21_Test.java
@@ -0,0 +1,14 @@
+package HW;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class HW21_Test extends BaseTest {
+    @Test
+    public void jenkinsTest() {
+
+    }
+}
