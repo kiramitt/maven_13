3238ce196f2254b2a73bcd87d8a900ac9631dc58
kiramitt <kiramitt@gmail.com>
2024-04-07 20:25:07 +0300

    /

--- src/test/java/Lessons/Lesson19Test.java
+++ src/test/java/Lessons/Lesson19Test.java
@@ -2,7 +2,4 @@ package Lessons;
 
 public class Lesson19Test {
 //    GitHub conflicts, CI/CD, Test framework (BaseTest)
-    public static void main(String[] args) {
-        System.out.println("GitHub conflicts, CI/CD, Test framework (BaseTest)");
-    }
 }
